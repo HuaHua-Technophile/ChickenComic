5d19fc2b95d62644f452c3517066afc2a611a48b
qfrx <1817721918@qq.com>
2023-06-02 21:49:15 +0800

    bug推送

--- src/views/SettingAbout.vue
+++ src/views/SettingAbout.vue
@@ -1,16 +1,11 @@
 <script setup lang="ts">
-  import { ref } from "vue";
-  // import BScroll from "@better-scroll/core";
-  // let bs = new BScroll({
-  //   click: true,
-  // });
+  import { ref, onMounted } from "vue";
 
   let settingAbout = ref(null);
-  console.log(settingAbout.value);
 
-  // watchEffect(() => {
-  //   console.log(checked.value);
-  // });
+  onMounted(() => {
+    console.log(settingAbout.value);
+  }),
 </script>
 
 <template>
