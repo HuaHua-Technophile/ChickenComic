1d07b467cb4a6aa45d6a2a573bbf6ab822cf5fb8
qfrx <1817721918@qq.com>
2023-06-03 10:00:01 +0800

    tuisong

--- src/views/SettingAbout.vue
+++ src/views/SettingAbout.vue
@@ -12,64 +12,33 @@
     });
   });
 
-  let { theme, changeTheme } = useGlobalStore();
+  let { theme, changeTheme }: any = useGlobalStore();
 </script>
 
 <template>
-  <div
-    ref="settingAbout"
-    class="settingAbout w-100 h-100 noScrollBar d-flex justify-content-center">
+  <div ref="settingAbout" class="settingAbout w-100 h-100 noScrollBar">
     <!-- 滚动内容 -->
-
-    <ul class="list-group fs-7 w-100 mx-4">
-      <li
-        class="d-flex align-items-center opacity-75 bg-light p-3 mt-5 rounded-4 d-flex justify-content-between">
-        <div class="des">
-          <h6 class="m-0">夜间模式</h6>
-          <p class="m-0 fs-10 opacity-75">开启夜间模式，夜间阅读不伤眼</p>
-        </div>
-        <div class="form-check form-switch">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            role="switch"
-            id="flexSwitchCheckChecked"
-            @click="changeTheme"
-            checked />
-        </div>
-      </li>
-      <li
-        class="d-flex align-items-center opacity-75 bg-light p-3 mt-5 rounded-4 d-flex justify-content-between">
-        <div class="des">
-          <h6 class="m-0">夜间模式</h6>
-          <p class="m-0 fs-10 opacity-75">开启夜间模式，夜间阅读不伤眼</p>
-        </div>
-        <div class="form-check form-switch">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            role="switch"
-            id="flexSwitchCheckChecked"
-            @click="changeTheme"
-            checked />
-        </div>
-      </li>
-      <li
-        class="d-flex align-items-center opacity-75 bg-light p-3 mt-5 rounded-4 d-flex justify-content-between">
-        <div class="des">
-          <h6 class="m-0">夜间模式</h6>
-          <p class="m-0 fs-10 opacity-75">开启夜间模式，夜间阅读不伤眼</p>
-        </div>
-        <div class="form-check form-switch">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            role="switch"
-            id="flexSwitchCheckChecked"
-            @click="changeTheme"
-            checked />
-        </div>
-      </li>
-    </ul>
+    <div
+      class="ps-3 pe-3 overflow-hidden"
+      style="--bs-bg-opacity: 0.2; min-height: 105vh">
+      <!-- 夜间模式切换 -->
+      <div
+        class="form-check form-switch pt-3 pb-3 bg-body-secondary rounded-pill"
+        style="margin-top: 300px; --bs-bg-opacity: 0.5">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          role="switch"
+          id="flexSwitchCheckDefault"
+          :checked="theme == 'dark'"
+          @click="changeTheme" />
+        <label class="form-check-label" for="flexSwitchCheckDefault"
+          >ナイトモードです</label
+        >
+      </div>
+      <!-- 关于 -->
+      <!-- 隐私协议 -->
+      <!-- 检查更新 -->
+    </div>
   </div>
 </template>
