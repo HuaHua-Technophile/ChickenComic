57d5bd81ccb796dcd3ebd00885cb28e30d19c554
qfrx <1817721918@qq.com>
2023-06-03 13:48:46 +0800

    // 添加并配置 vant4 样式自动引入插件

---
+++ src/views/SearchInput.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import { ref } from "vue";
+  import { showToast } from "vant";
+  const value = ref("");
+  const onSearch = (val: any) => showToast(val);
+  const onCancel = () => showToast("取消");
+</script>
+
+<template>
+  <div ref="SearchInput" class="SearchInput w-100 h-100 noScrollBar">
+    <form action="/">
+      <van-search
+        v-model="value"
+        show-action
+        placeholder="请输入搜索关键词"
+        @search="onSearch"
+        @cancel="onCancel"
+        background="transparent"
+        :shape="'round'" />
+    </form>
+  </div>
+</template>
+<style lang="scss">
+  .SearchInput {
+    // color: ;
+  }
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,24 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+// 引入vant样式按需自动引入组件
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({ // vant样式按需自动引入组件
+      resolvers: [VantResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  
   // 配置代理，解决跨域请求问题
   // server: {
   //   proxy: {
