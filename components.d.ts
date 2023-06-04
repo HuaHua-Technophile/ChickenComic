26391ec45aede60370e6edabf5e2f1d07b24ac44
qfrx <1817721918@qq.com>
2023-06-04 20:02:44 +0800

    // 搜索页面：历史搜索，热门搜索

--- components.d.ts
+++ components.d.ts
@@ -11,5 +11,6 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    VanSearch: typeof import('vant/es')['Search']
   }
 }
