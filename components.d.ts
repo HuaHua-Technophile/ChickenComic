b1a0b0bc7d003a2f08cf6feb66ae918d4dd4bb94
qfrx <1817721918@qq.com>
2023-06-10 15:06:39 +0800

    // 提交

--- components.d.ts
+++ components.d.ts
@@ -5,15 +5,17 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 import '@vue/runtime-core'
 
-export {}
+export {};
 
-declare module '@vue/runtime-core' {
+declare module "vue" {
   export interface GlobalComponents {
-    ChapterComponent: typeof import('./src/components/chapterComponent.vue')['default']
-    ElInput: typeof import('element-plus/es')['ElInput']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
-    VanTab: typeof import('vant/es')['Tab']
-    VanTabs: typeof import('vant/es')['Tabs']
+    ChapterComponent: typeof import("./src/components/chapterComponent.vue")["default"];
+    ElInput: typeof import("element-plus/es")["ElInput"];
+    RouterLink: typeof import("vue-router")["RouterLink"];
+    RouterView: typeof import("vue-router")["RouterView"];
+    VanDropdownItem: typeof import("vant/es")["DropdownItem"];
+    VanDropdownMenu: typeof import("vant/es")["DropdownMenu"];
+    VanLoading: typeof import("vant/es")["Loading"];
+    VanSearch: typeof import("vant/es")["Search"];
   }
 }
