f39840fee0ae32d10e5556046c0c309a2aebe4e3
nikaidoo-shinku <1192490160@qq.com>
2023-06-20 11:31:08 +0800

    优化悬浮球实现代码

--- components.d.ts
+++ components.d.ts
@@ -5,24 +5,20 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 import '@vue/runtime-core'
 
-export {}
+export {};
 
-declare module '@vue/runtime-core' {
+declare module "@vue/runtime-core" {
   export interface GlobalComponents {
-    BackComponent: typeof import('./src/components/backComponent.vue')['default']
-    ChapterComponent: typeof import('./src/components/chapterComponent.vue')['default']
-    ComicItemComponent: typeof import('./src/components/comicItemComponent.vue')['default']
-    ComicReadBar: typeof import('./src/components/ComicReadBar.vue')['default']
-    RankingBar: typeof import('./src/components/RankingBar.vue')['default']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
-    SearchBar: typeof import('./src/components/SearchBar.vue')['default']
-    UserSetting: typeof import('./src/components/UserSetting.vue')['default']
-<<<<<<< HEAD
-    VanOverlay: typeof import('vant/es')['Overlay']
-=======
-    VanSearch: typeof import('vant/es')['Search']
->>>>>>> a903968e1fdb1ad5891d6322e509211b070ee5d6
-    VanSlider: typeof import('vant/es')['Slider']
+    BackComponent: typeof import("./src/components/backComponent.vue")["default"];
+    ChapterComponent: typeof import("./src/components/chapterComponent.vue")["default"];
+    ComicItemComponent: typeof import("./src/components/comicItemComponent.vue")["default"];
+    ComicReadBar: typeof import("./src/components/ComicReadBar.vue")["default"];
+    RankingBar: typeof import("./src/components/RankingBar.vue")["default"];
+    RouterLink: typeof import("vue-router")["RouterLink"];
+    RouterView: typeof import("vue-router")["RouterView"];
+    SearchBar: typeof import("./src/components/SearchBar.vue")["default"];
+    UserSetting: typeof import("./src/components/UserSetting.vue")["default"];
+    VanOverlay: typeof import("vant/es")["Overlay"];
+    VanSlider: typeof import("vant/es")["Slider"];
   }
 }
