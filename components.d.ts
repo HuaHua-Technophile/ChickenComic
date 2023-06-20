cdc3aac99d5ea561afae85ce73496a91eea189fa
nikaidoo-shinku <1192490160@qq.com>
2023-06-20 14:05:44 +0800

    当前在读传参修改

--- components.d.ts
+++ components.d.ts
@@ -5,20 +5,21 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 import '@vue/runtime-core'
 
-export {};
+export {}
 
-declare module "@vue/runtime-core" {
+declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    BackComponent: typeof import("./src/components/backComponent.vue")["default"];
-    ChapterComponent: typeof import("./src/components/chapterComponent.vue")["default"];
-    ComicItemComponent: typeof import("./src/components/comicItemComponent.vue")["default"];
-    ComicReadBar: typeof import("./src/components/ComicReadBar.vue")["default"];
-    RankingBar: typeof import("./src/components/RankingBar.vue")["default"];
-    RouterLink: typeof import("vue-router")["RouterLink"];
-    RouterView: typeof import("vue-router")["RouterView"];
-    SearchBar: typeof import("./src/components/SearchBar.vue")["default"];
-    UserSetting: typeof import("./src/components/UserSetting.vue")["default"];
-    VanOverlay: typeof import("vant/es")["Overlay"];
-    VanSlider: typeof import("vant/es")["Slider"];
+    BackComponent: typeof import('./src/components/backComponent.vue')['default']
+    ChapterComponent: typeof import('./src/components/chapterComponent.vue')['default']
+    ComicItemComponent: typeof import('./src/components/comicItemComponent.vue')['default']
+    ComicReadBar: typeof import('./src/components/ComicReadBar.vue')['default']
+    RankingBar: typeof import('./src/components/RankingBar.vue')['default']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
+    SearchBar: typeof import('./src/components/SearchBar.vue')['default']
+    UserSetting: typeof import('./src/components/UserSetting.vue')['default']
+    VanOverlay: typeof import("vant/es")["Overlay"]
+    VanSearch: typeof import('vant/es')['Search']
+    VanSlider: typeof import('vant/es')['Slider']
   }
 }
